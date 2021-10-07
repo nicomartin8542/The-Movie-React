3c3cb1c045b7be2b4e7f65a35c2948c6893bb618
fedeherrera10 <herrera3299@gmail.com>
2021-10-07 14:27:33 -0300

    Agregando state al boton de menu

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "./SideBar";
 
 const Header = () => {
+
+  const [openmenu,setOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpen(!openmenu);
+  }
+
+  const classBgLayout = (openmenu) ? 'bg-layout bg-layout-active' : 'bg-layout';
+
   return (
     <header className="d-flex">
       <div className="header__logo">
         <h1>ReactMovie </h1>
         <i className="fa fa-film " aria-hidden="true"></i>
       </div>
-      <SideBar/>
+      <SideBar openMenu={openmenu}/>
       <button
         className="btn-menu"
+        onClick={handleOpenMenu}
       >
         <i
           className="fa fa-bars fa-2x"
@@ -18,7 +28,7 @@ const Header = () => {
         </i>
       </button>
 
-      <div className="bg-layout"></div>
+      <div className={classBgLayout}></div>
 
     </header>
   );
--- src/components/layouts/SideBar.jsx
+++ src/components/layouts/SideBar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Search } from "../Search";
 
-const SideBar = () => {
-  return (
-    <aside>
+const SideBar = ({openMenu}) => {
+  const classOpen = (openMenu) ? 'aside-open' :'aside-close';
+
+return (
+    <aside className={classOpen}>
       <Search/>
       <li className="logout"><i className="fa fa-sign-out" aria-hidden="true"></i>&nbsp;Logout</li>
     </aside>
