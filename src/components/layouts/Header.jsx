4ccaf6d77ee1fa4e0bbe7323854b0d0187a51c26
fedeherrera10 <herrera3299@gmail.com>
2021-10-14 15:40:06 -0300

    Agregado de biblioteca animatecss y se aplica animacion al navbar

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,23 +4,33 @@ import Navbar from "./Navbar";
 const Header = () => {
 
   const [openmenu,setOpen] = useState(false);
+  const [classanimation,setClassAnimation] = useState('');
+
   const handleOpenMenu = () => {
     setOpen(!openmenu);
+    let classAnim  = (!openmenu) ? 'animate__slideInLeft' : 'animate__slideOutLeft';
+    setClassAnimation(classAnim);
   }
+
   const classBgLayout = (openmenu) ? 'bg-layout bg-layout-active' : 'bg-layout';
+  const iconMenu = (!openmenu) ? 'fa-bars' : 'fa-times';
+
 
   return (
     <header className="d-flex">
       <div className="header__logo">
-        <h1>ReactMovie </h1>
-        <i className="fa fa-film " aria-hidden="true"></i>
+        <h1>
+            ReactMovie
+            <i className="fa fa-film " aria-hidden="true"></i>
+        </h1>
+
       </div>
-      <Navbar openMenu={openmenu}/>
+      <Navbar openMenu={openmenu} classanimation={classanimation}/>
       <button
         className="btn-menu"
         onClick={handleOpenMenu}
       >
-        <i className="fa fa-bars fa-2x"aria-hidden="true" ></i>
+      <i className={`fa ${iconMenu} fa-2x`} aria-hidden="true"></i>
       </button>
       <div className={classBgLayout}></div>
     </header>
--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Search } from "../search/Search";
 
 
-const Navbar = ({openMenu}) => {
-  const classOpen = (openMenu) ? 'nav-open' :'nav-close';
+const Navbar = ({openMenu,classanimation}) => {
+
+const classOpen = (openMenu) ? `nav-open ${classanimation}`  :`nav-close ${classanimation}`;
+
+const handleAnimationEnd = (e) => {
+  if(e.animationName === 'slideOutLeft'){
+    e.target.classList.remove('animate__slideOutLeft');
+  }
+}
 
 return (
-    <nav className={classOpen}>
+    <nav className={`animate__animated ${classOpen}`} onAnimationEnd={handleAnimationEnd} >
       <Search/>
       <li className="logout"><i className="fa fa-sign-out" aria-hidden="true"></i>&nbsp;Logout</li>
     </nav>
