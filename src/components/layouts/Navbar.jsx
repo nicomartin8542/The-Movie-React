dd31fd7e6034181c4c0158121828470e89656fe7
Jorge Nicolas Martin <nicomartin854@gmail.com>
2021-10-15 00:04:01 -0300

    Agregado del nombre del usuario en el home

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,32 +1,35 @@
-import React, { useContext } from "react";
+import React, {useContext} from "react";
 import FirebaseContext from "../../firebase/context";
-import { useHistory } from "react-router-dom";
-import { Search } from "../search/Search";
+import {useHistory} from "react-router-dom";
+import {Search} from "../search/Search";
 
-const Navbar = ({ openMenu }) => {
-  const classOpen = openMenu ? "nav-open" : "nav-close";
+const Navbar = ({openMenu}) => {
+	const classOpen = openMenu ? "nav-open" : "nav-close";
 
-  //Utilizo el context de firebase
-  const firebaseContext = useContext(FirebaseContext);
-  const { firebase } = firebaseContext;
+	//Utilizo el context de firebase
+	const firebaseContext = useContext(FirebaseContext);
+	const {firebase, usuario} = firebaseContext;
 
-  //Utilizo el history del react router dom
-  const history = useHistory();
+	console.log(usuario.displayName);
 
-  //Cerrar sesion
-  const logout = async () => {
-    await firebase.logout();
-    history.push("/");
-  };
+	//Utilizo el history del react router dom
+	const history = useHistory();
 
-  return (
-    <nav className={classOpen}>
-      <Search />
-      <li className="logout" onClick={() => logout()}>
-        <i className="fa fa-sign-out" aria-hidden="true"></i>&nbsp;Logout
-      </li>
-    </nav>
-  );
+	//Cerrar sesion
+	const logout = async () => {
+		await firebase.logout();
+		history.push("/");
+	};
+
+	return (
+		<nav className={classOpen}>
+			<Search />
+			<p className="user-name">Bienvenido, {usuario.displayName}</p>
+			<li className="logout" onClick={() => logout()}>
+				<i className="fa fa-sign-out" aria-hidden="true"></i>&nbsp;Logout
+			</li>
+		</nav>
+	);
 };
 
 export default Navbar;
